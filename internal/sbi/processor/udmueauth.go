1aecc0980c134dfcaa32be957c2ad35c6e8c86e8
宋哲頤 <72761211+sungchi127@users.noreply.github.com>
2024-12-05 21:09:32 +0800

    Update udmueauth.go

--- internal/sbi/processor/udmueauth.go
+++ internal/sbi/processor/udmueauth.go
@@ -1,7 +1,9 @@
 package processor
 
 import (
-	"net/http"
+    "fmt"
+    "net/http"
+    
 
 	"github.com/free5gc/openapi/models"
 	"github.com/free5gc/scp/internal/logger"
@@ -14,14 +16,36 @@ func (p *Processor) PostGenerateAuthData(
 ) *HandlerResponse {
 	logger.ProxyLog.Debugln("[AUSF->UDM] Forward AUSF UE Authentication Request")
 
+
+    // 驗證 supiOrSuci
+    if supiOrSuci == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.SupiOrSuci: Mandatory type is absent")
+        problemDetails := &models.ProblemDetails{
+            Status: http.StatusBadRequest,
+            Cause:  "INVALID_REQUEST",
+            Detail: "missing supiOrSuci",
+        }
+        return &HandlerResponse{http.StatusBadRequest, nil, problemDetails}
+    }
+
+    // 驗證 AuthenticationInfoRequest
+    if err := validateAuthInfoRequest(authInfo); err != nil {
+        problemDetails := &models.ProblemDetails{
+            Status: http.StatusBadRequest,
+            Cause:  "INVALID_REQUEST",
+            Detail: err.Error(),
+        }
+        return &HandlerResponse{http.StatusBadRequest, nil, problemDetails}
+    }
+
 	// TODO: Send request to target NF by setting correct uri
 	var targetNfUri string
 	targetNfUri = "http://10.100.200.3:8000"
 
+	
 	// TODO: Verify that the Information Elements (IEs) in the request or response body are correct
 	//       Recover and handle errors if the IEs are incorrect
 	response, problemDetails, err := p.Consumer().SendGenerateAuthDataRequest(targetNfUri, supiOrSuci, &authInfo)
-	// logger.ProxyLog.Debugf("response: %#v, problemDetails: %#v, err: %s", response, problemDetails, err)
 
 	if response != nil {
 		return &HandlerResponse{http.StatusOK, nil, response}
@@ -35,4 +59,37 @@ func (p *Processor) PostGenerateAuthData(
 	}
 
 	return &HandlerResponse{http.StatusForbidden, nil, problemDetails}
+}
+
+// 驗證 AuthenticationInfoRequest
+func validateAuthInfoRequest(authInfo models.AuthenticationInfoRequest) error {
+    // 檢查必要欄位
+    if authInfo.ServingNetworkName == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.ServingNetworkName: Mandatory type is absent")
+        return fmt.Errorf("missing mandatory field: servingNetworkName")
+    }
+
+    if authInfo.AusfInstanceId == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.AusfInstanceId: Mandatory type is absent")
+        return fmt.Errorf("missing mandatory field: ausfInstanceId")
+    }
+
+    // 驗證欄位值
+    /*if !strings.HasPrefix(authInfo.AusfInstanceId, "ausf-") {
+        logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.AusfInstanceId: Unexpected value is received")
+        return fmt.Errorf("invalid ausfInstanceId format")
+    }*/
+    // 檢查條件性欄位
+    if authInfo.ResynchronizationInfo != nil {
+        if authInfo.ResynchronizationInfo.Rand == "" {
+            logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.ResynchronizationInfo.Rand: Miss condition")
+            return fmt.Errorf("rand is required when resynchronizationInfo is present")
+        }
+        if authInfo.ResynchronizationInfo.Auts == "" {
+            logger.DetectorLog.Errorln("models.AuthenticationInfoRequest.ResynchronizationInfo.Auts: Miss condition") 
+            return fmt.Errorf("auts is required when resynchronizationInfo is present")
+        }
+    }
+
+    return nil
 }
