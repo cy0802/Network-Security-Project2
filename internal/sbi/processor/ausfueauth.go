abe6d7ec2a7bef9a0a8a91c3890eab16ba890ab5
宋哲頤 <72761211+sungchi127@users.noreply.github.com>
2024-12-05 21:08:51 +0800

    Update ausfueauth.go

--- internal/sbi/processor/ausfueauth.go
+++ internal/sbi/processor/ausfueauth.go
@@ -1,7 +1,9 @@
 package processor
 
 import (
-	"net/http"
+    "fmt"
+    "net/http"
+    
 
 	"github.com/free5gc/openapi/models"
 	"github.com/free5gc/scp/internal/logger"
@@ -13,14 +15,23 @@ func (p *Processor) PostUeAutentications(
 ) *HandlerResponse {
 	logger.ProxyLog.Debugln("[AMF->AUSF] Forward AMF UE Authentication Request")
 
+    // 驗證 Information Elements
+    if err := validateAuthInfo(authInfo); err != nil {
+        problemDetails := &models.ProblemDetails{
+            Status: http.StatusBadRequest,
+            Cause:  "INVALID_REQUEST",
+            Detail: err.Error(),
+        }
+        return &HandlerResponse{http.StatusBadRequest, nil, problemDetails}
+    }
+
 	// TODO: Send request to target NF by setting correct uri
 	var targetNfUri string
-	targetNfUri = "http://10.100.200.9:8000"
+    targetNfUri = "http://10.100.200.9:8000"
 
 	// TODO: Verify that the Information Elements (IEs) in the response body are correct
 	//       Recover and handle errors if the IEs are incorrect
 	response, problemDetails, err := p.Consumer().SendUeAuthPostRequest(targetNfUri, &authInfo)
-	logger.ProxyLog.Debugf("response: %#v, problemDetails: %#v, err: %s", response, problemDetails, err)
 
 	if response != nil {
 		return &HandlerResponse{http.StatusCreated, nil, response}
@@ -42,14 +53,36 @@ func (p *Processor) PutUeAutenticationsConfirmation(
 ) *HandlerResponse {
 	logger.ProxyLog.Debugln("[AMF->AUSF] Forward AMF UE Authentication Response")
 
+    // 驗證 authCtxId
+    if authCtxId == "" {
+        logger.DetectorLog.Errorln("models.ConfirmationData.AuthCtxId: Mandatory type is absent")
+        problemDetails := &models.ProblemDetails{
+            Status: http.StatusBadRequest,
+            Cause:  "INVALID_REQUEST",
+            Detail: "missing authCtxId",
+        }
+        return &HandlerResponse{http.StatusBadRequest, nil, problemDetails}
+    }
+
+    // 驗證 ConfirmationData
+    if err := validateConfirmationData(confirmationData); err != nil {
+        problemDetails := &models.ProblemDetails{
+            Status: http.StatusBadRequest,
+            Cause:  "INVALID_REQUEST",
+            Detail: err.Error(),
+        }
+        return &HandlerResponse{http.StatusBadRequest, nil, problemDetails}
+    }
+
 	// TODO: Send request to target NF by setting correct uri
 	var targetNfUri string
 	targetNfUri = "http://10.100.200.9:8000"
 
 	// TODO: Verify that the Information Elements (IEs) in the response body are correct
 	//       Recover and handle errors if the IEs are incorrect
+	
 	response, problemDetails, err := p.Consumer().SendAuth5gAkaConfirmRequest(targetNfUri, authCtxId, &confirmationData)
-	// logger.ProxyLog.Debugf("response: %#v, problemDetails: %#v, err: %s", response, problemDetails, err)
+
 	if response != nil {
 		return &HandlerResponse{http.StatusOK, nil, response}
 	} else if problemDetails != nil {
@@ -62,4 +95,49 @@ func (p *Processor) PutUeAutenticationsConfirmation(
 	}
 
 	return &HandlerResponse{http.StatusForbidden, nil, problemDetails}
+}
+
+// 驗證 AuthenticationInfo
+func validateAuthInfo(authInfo models.AuthenticationInfo) error {
+    // 檢查必要欄位
+    if authInfo.SupiOrSuci == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfo.SupiOrSuci: Mandatory type is absent")
+        return fmt.Errorf("missing mandatory field: supiOrSuci")
+    }
+
+    if authInfo.ServingNetworkName == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfo.ServingNetworkName: Mandatory type is absent")
+        return fmt.Errorf("missing mandatory field: servingNetworkName")
+    }
+
+    // 驗證欄位值
+    /*if !strings.HasPrefix(authInfo.SupiOrSuci, "5G:") {
+       logger.DetectorLog.Errorln("models.AuthenticationInfo.SupiOrSuci: Unexpected value is received")
+        return fmt.Errorf("invalid supiOrSuci format")
+    }*/
+
+    // 檢查條件性欄位
+    if authInfo.ResynchronizationInfo != nil && authInfo.ResynchronizationInfo.Rand == "" {
+        logger.DetectorLog.Errorln("models.AuthenticationInfo.ResynchronizationInfo.Rand: Miss condition")
+        return fmt.Errorf("rand is required when resynchronizationInfo is present")
+    }
+
+    return nil
+}
+
+// 驗證 ConfirmationData
+func validateConfirmationData(data models.ConfirmationData) error {
+    // 檢查必要欄位
+    if data.ResStar == "" {
+        logger.DetectorLog.Errorln("models.ConfirmationData.ResStar: Mandatory type is absent")
+        return fmt.Errorf("missing mandatory field: resStar")
+    }
+
+    // 驗證欄位值
+    /*if len(data.ResStar) != 64 {
+        logger.DetectorLog.Errorln("models.ConfirmationData.ResStar: Unexpected value is received")
+        return fmt.Errorf("invalid resStar length: expected 64 characters")
+    }*/
+
+    return nil
 }
